9d922ae8d6bb3a74fb1495713ddde966300e5b01
wpl <2000012913@stu.pku.edu.cn>
2023-08-28 15:12:00 +0800

    lab4 PartB down!

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,14 +1,15 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
+ */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+       if(n%2 == 1)
+           n = 3*n+1;
+       else
+           n = n/2;
+       return n;
     }
 
     public static void main(String[] args) {
@@ -21,3 +22,4 @@ public class Collatz {
         System.out.println();
     }
 }
+
