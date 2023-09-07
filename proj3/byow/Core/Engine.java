530d00452f0cd2f59d71dac2635c0a8071f26fdd
wpl <2000012913@stu.pku.edu.cn>
2023-09-07 23:17:18 +0800

    proj3 phase1 debug1!

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -45,7 +45,7 @@ public class Engine {
         //
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
         // that works for many different input types.
-        if(input.charAt(0)== 'N')
+        if(input.charAt(0)== 'n')
         {
             int i = input.indexOf("s");
             String str = input.substring(1,i);
